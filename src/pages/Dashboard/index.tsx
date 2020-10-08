84c5d0e179384c63429cbf958b44fd79739206f0
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2020-10-08 11:58:10 -0300

    feat: add routes system private and public routes

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View } from 'react-native';
+import Button from '../../components/Button';
+
+import { useAuth } from '../../hooks/auth';
+
+const Dashboard: React.FC = () => {
+  const { signOut } = useAuth();
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 30,
+      }}
+    >
+      <Button onPress={signOut}>Sair</Button>
+    </View>
+  );
+};
+
+export default Dashboard;
